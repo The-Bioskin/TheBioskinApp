cedc86ec944de645237b5320ba8ee056d4d24609
conmeobietbay <hueptt20411c@st.uel.edu.vn>
2022-11-27 21:05:30 +0700

    add code interface of show product detail

---
+++ app/src/main/java/com/example/thebioskin/ShowProductDetail.java
@@ -0,0 +1,14 @@
+package com.example.thebioskin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShowProductDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_product_detail);
+    }
+}
