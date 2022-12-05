924fd495570697184b8a1b6da2222ded489e801f
Junnam2806 <namtruong286@gmail.com>
2022-12-05 15:08:08 +0700

    More hot product feature

--- app/src/main/java/com/example/thebioskin/HomeActivity.java
+++ app/src/main/java/com/example/thebioskin/HomeActivity.java
@@ -3,27 +3,57 @@ package com.example.thebioskin;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.constraintlayout.widget.ConstraintSet;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ListView;
 
-public class HomeActivity extends AppCompatActivity {
-    ConstraintLayout ct9, ct7;
+import com.example.adapter.HotProductAdapter;
+import com.example.adapter.ProductAdapter;
+import com.example.models.HotProduct;
+
+import java.util.ArrayList;
+
+public class HomeActivity extends AppCompatActivity implements HotProductAdapter.ViewHolder.OnHotProductListener {
+    ConstraintLayout ct9, ct7, ct2;
     EditText searchProduct;
+    RecyclerView rclHotProduct;
+    ArrayList<HotProduct> hotProductsItems;
+    HotProductAdapter hotProductAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         linkViews();
         addEvents();
+        addViews();
+    }
+
+    private void addViews() {
+        rclHotProduct = findViewById(R.id.rclPopularProduct);
+        hotProductsItems = new ArrayList<>();
+        hotProductsItems.add(new HotProduct(R.drawable.nuoc_tay_trang_effaclar, "465.000 đ", "Nước tẩy trang Effaclar Micellar Water Ultra Oily Skin cho da dầu, mụn nhạy cảm 400ml", "4/5", "128 ratings"));
+        hotProductsItems.add(new HotProduct(R.drawable.nuoc_tay_trang_effaclar, "465.000 đ", "Nước tẩy trang Effaclar Micellar Water Ultra Oily Skin cho da dầu, mụn nhạy cảm 400ml", "4/5", "128 ratings"));
+        hotProductsItems.add(new HotProduct(R.drawable.nuoc_tay_trang_effaclar, "465.000 đ", "Nước tẩy trang Effaclar Micellar Water Ultra Oily Skin cho da dầu, mụn nhạy cảm 400ml", "4/5", "128 ratings"));
+        hotProductsItems.add(new HotProduct(R.drawable.nuoc_tay_trang_effaclar, "465.000 đ", "Nước tẩy trang Effaclar Micellar Water Ultra Oily Skin cho da dầu, mụn nhạy cảm 400ml", "4/5", "128 ratings"));
+        hotProductsItems.add(new HotProduct(R.drawable.nuoc_tay_trang_effaclar, "465.000 đ", "Nước tẩy trang Effaclar Micellar Water Ultra Oily Skin cho da dầu, mụn nhạy cảm 400ml", "4/5", "128 ratings"));
+
+        hotProductAdapter = new HotProductAdapter(getApplicationContext(),hotProductsItems,this);
+        rclHotProduct.hasFixedSize();
+        rclHotProduct.setAdapter(hotProductAdapter);
+        LinearLayoutManager linearLayoutManager1 = new LinearLayoutManager(HomeActivity.this, RecyclerView.HORIZONTAL, false);
+        rclHotProduct.setLayoutManager(linearLayoutManager1);
     }
 
 
     private void linkViews() {
         ct9 = findViewById(R.id.ct_9);
         ct7 = findViewById(R.id.ct_7);
+        ct2 = findViewById(R.id.ct_2);
         searchProduct = findViewById(R.id.edt_SearchedProduct);
     }
 
@@ -49,5 +79,17 @@ public class HomeActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        ct2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeActivity.this, MoreHotProduct.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public void OnHotProductClick(int position) {
+
     }
 }
--- app/src/main/java/com/example/thebioskin/SearchProductActivity.java
+++ app/src/main/java/com/example/thebioskin/SearchProductActivity.java
@@ -11,26 +11,34 @@ import android.widget.ImageView;
 import java.time.chrono.HijrahChronology;
 
 public class SearchProductActivity extends AppCompatActivity {
-//    ImageView imvBack;
+    ImageView imvCancel, imvFilter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_product);
-//        linkViews();
-//        addEvents();
+        linkViews();
+        addEvents();
     }
 
-//    private void addEvents() {
-//        imvBack.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent = new Intent(SearchProductActivity.this, HomeActivity.class);
-//                startActivity(intent);
-//            }
-//        });
-//    }
-//
-//    private void linkViews() {
-//        imvBack = findViewById(R.id.imvBack);
-//    }
+    private void addEvents() {
+        imvCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SearchProductActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+        imvFilter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SearchProductActivity.this, Filter.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void linkViews() {
+        imvCancel = findViewById(R.id.imvBack);
+        imvFilter = findViewById(R.id.imv_Filter);
+    }
 }
